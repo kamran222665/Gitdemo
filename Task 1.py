7e4ca5aa4a0f654a696cfbc015a3b76639b4e4fe
kamran222665 <kamran222665@gmail.com>
2025-10-23 14:37:59 +0530

    Initial Commit

---
+++ Task 1.py
@@ -0,0 +1,12 @@
+"""Perform Basic Mathematical Operations"""
+
+a=float(input("Enter first number: "))
+b=float(input("Enter second number: "))
+addition=a+b
+sub=a-b
+multiplication=a*b
+divide=a/b
+print("Addition= ",addition)
+print("Subtraction= ",sub)
+print("Multiplication= ",multiplication)
+print("Division= ",divide)
---
+++ Task2.py
@@ -0,0 +1,5 @@
+"""Create a Personalized Greeting"""
+
+first=input('Enter your first name: ')
+last=input('Enter your last name: ')
+print("Hello,",first, last+"!","Welcome to the Python program.")
